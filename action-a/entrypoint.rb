38f42f0b83ceed495dc519d561dfaaaec3c903ce
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-07-19 17:40:23 -0600

    .rb to .sh

--- action-a/entrypoint.rb
+++
@@ -1,3 +0,0 @@
-#!/bin/ruby -l
-
-`echo Hello world my name is $INPUT_MY_NAME and your name is $INPUT_YOUR_NAME`
---
+++ action-a/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -l
+
+exec -c "echo Hello world my name is $INPUT_MY_NAME and your name is $INPUT_YOUR_NAME"
