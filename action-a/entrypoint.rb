deab4aaaefc72d642cf18aedc76056afd73c3c21
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-07-19 17:33:53 -0600

    update to ruby, next we can try go maybe

---
+++ action-a/entrypoint.rb
@@ -0,0 +1,3 @@
+#!/bin/ruby -l
+
+`echo Hello world my name is $INPUT_MY_NAME and your name is $INPUT_YOUR_NAME`
--- action-a/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -l
-
-sh -c "echo Hello world my name is $INPUT_MY_NAME and your name is $INPUT_YOUR_NAME"
